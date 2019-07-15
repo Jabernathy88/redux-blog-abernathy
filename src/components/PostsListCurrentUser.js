ece9674537d6ca527885134fd8912120b3e2a5ff
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-15 13:41:19 -0400

    Display read Users success (hard-coded current user 1).

--- src/components/PostsListCurrentUser.js
+++ src/components/PostsListCurrentUser.js
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../userActions';
 
-export default class PostsListCurrentUser extends Component {
+export class PostsListCurrentUser extends Component {
+  componentDidMount() {
+    if (this.props.dispatch) {
+      this.props.dispatch(fetchUsers());
+    }
+  }
+
   render() {
+    const { error, loading, users } = this.props;
+
+    if (error) {
+      return <div>Error: {error.message}</div>
+    }
+    if (loading) {
+      return <div>Loading ...</div>;
+    }
+
     return (
-      <div>
+      <div className="posts-list-current-user">
         Hello from Current User Posts
+        {users && users.map(user => (
+          <div key={user.id}>
+            <p><small>User #{user.id}:</small></p>
+            <p><strong>{user.name}</strong></p>
+          </div>
+        ))}
       </div>
     )
   }
 }
 
+const mapStateToProps = state => ({
+  users: state.users.items,
+  loading: state.users.loading,
+  error: state.users.error
+})
 
+export default connect(mapStateToProps)(PostsListCurrentUser);
