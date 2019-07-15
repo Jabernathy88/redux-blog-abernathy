5b7ad12ecca82f6d8a5cdd74de3096ffc36668d8
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-15 13:53:25 -0400

    Display read Users success (hard-coded current user 1).

--- src/components/PostsListCurrentUser.js
+++ src/components/PostsListCurrentUser.js
@@ -12,6 +12,8 @@ export class PostsListCurrentUser extends Component {
   render() {
     const { error, loading, users } = this.props;
 
+    const currentUser = users[0]
+
     if (error) {
       return <div>Error: {error.message}</div>
     }
@@ -28,6 +30,9 @@ export class PostsListCurrentUser extends Component {
             <p><strong>{user.name}</strong></p>
           </div>
         ))}
+        { // temp log
+          currentUser && console.log("I am current user:", currentUser)
+        }
       </div>
     )
   }
