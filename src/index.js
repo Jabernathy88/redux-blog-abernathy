fd6c365bf051861ba61e16e279c2415c22cec454
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-08-07 20:50:35 -0400

    Small cleanup.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import rootReducer from "./rootReducer";
+import rootReducer from "./reducers/rootReducer";
 
 const store = createStore(
   rootReducer,
